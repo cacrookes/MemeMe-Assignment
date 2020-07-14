b46d40da37ea5a685eea40087190948ce56291a4
Christopher Crookes <cacrookes@gmail.com>
2020-07-14 14:06:40 -0700

    Adding Meme struct file.

---
+++ MemeMe Assignment/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe Assignment
+//
+//  Created by Christopher Crookes on 2020-07-14.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memeImage: UIImage
+}
