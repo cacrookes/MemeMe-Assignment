29b925fd3df6fbaa1b46c6c167eb1e968c366c00
Christopher Crookes <cacrookes@gmail.com>
2020-07-17 17:37:55 -0700

    Adding MemeDetailViewController.

---
+++ MemeMe Assignment/Controllers/MemeDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe Assignment
+//
+//  Created by Christopher Crookes on 2020-07-17.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    @IBOutlet weak var memeImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
